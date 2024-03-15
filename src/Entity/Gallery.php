571108b44635de3b7f090f10e7d234d6bd8c4935
Mathieu <mathieu.montjarret@icloud.com>
2024-03-15 11:51:35 +0100

    Feat: Add entity and repository

---
+++ src/Entity/Gallery.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\GalleryRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: GalleryRepository::class)]
+class Gallery
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Name = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $PublishedAt = null;
+
+    #[ORM\OneToMany(targetEntity: Photo::class, mappedBy: 'Gallery')]
+    private Collection $photos;
+
+    #[ORM\ManyToMany(targetEntity: Service::class, inversedBy: 'galleries')]
+    private Collection $Services;
+
+    #[ORM\ManyToMany(targetEntity: User::class, inversedBy: 'galleries')]
+    private Collection $Users;
+
+    public function __construct()
+    {
+        $this->photos = new ArrayCollection();
+        $this->Services = new ArrayCollection();
+        $this->Users = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->Name;
+    }
+
+    public function setName(string $Name): static
+    {
+        $this->Name = $Name;
+
+        return $this;
+    }
+
+    public function getPublishedAt(): ?\DateTimeImmutable
+    {
+        return $this->PublishedAt;
+    }
+
+    public function setPublishedAt(\DateTimeImmutable $PublishedAt): static
+    {
+        $this->PublishedAt = $PublishedAt;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Photo>
+     */
+    public function getPhotos(): Collection
+    {
+        return $this->photos;
+    }
+
+    public function addPhoto(Photo $photo): static
+    {
+        if (!$this->photos->contains($photo)) {
+            $this->photos->add($photo);
+            $photo->setGallery($this);
+        }
+
+        return $this;
+    }
+
+    public function removePhoto(Photo $photo): static
+    {
+        if ($this->photos->removeElement($photo)) {
+            // set the owning side to null (unless already changed)
+            if ($photo->getGallery() === $this) {
+                $photo->setGallery(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Service>
+     */
+    public function getServices(): Collection
+    {
+        return $this->Services;
+    }
+
+    public function addService(Service $service): static
+    {
+        if (!$this->Services->contains($service)) {
+            $this->Services->add($service);
+        }
+
+        return $this;
+    }
+
+    public function removeService(Service $service): static
+    {
+        $this->Services->removeElement($service);
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, User>
+     */
+    public function getUsers(): Collection
+    {
+        return $this->Users;
+    }
+
+    public function addUser(User $user): static
+    {
+        if (!$this->Users->contains($user)) {
+            $this->Users->add($user);
+        }
+
+        return $this;
+    }
+
+    public function removeUser(User $user): static
+    {
+        $this->Users->removeElement($user);
+
+        return $this;
+    }
+}
---
+++ src/Entity/Service.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ServiceRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ServiceRepository::class)]
+class Service
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Title = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $Information = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 10, scale: 2)]
+    private ?string $Price = null;
+
+    #[ORM\ManyToMany(targetEntity: Gallery::class, mappedBy: 'Services')]
+    private Collection $galleries;
+
+    public function __construct()
+    {
+        $this->galleries = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->Title;
+    }
+
+    public function setTitle(string $Title): static
+    {
+        $this->Title = $Title;
+
+        return $this;
+    }
+
+    public function getInformation(): ?string
+    {
+        return $this->Information;
+    }
+
+    public function setInformation(string $Information): static
+    {
+        $this->Information = $Information;
+
+        return $this;
+    }
+
+    public function getPrice(): ?string
+    {
+        return $this->Price;
+    }
+
+    public function setPrice(string $Price): static
+    {
+        $this->Price = $Price;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Gallery>
+     */
+    public function getGalleries(): Collection
+    {
+        return $this->galleries;
+    }
+
+    public function addGallery(Gallery $gallery): static
+    {
+        if (!$this->galleries->contains($gallery)) {
+            $this->galleries->add($gallery);
+            $gallery->addService($this);
+        }
+
+        return $this;
+    }
+
+    public function removeGallery(Gallery $gallery): static
+    {
+        if ($this->galleries->removeElement($gallery)) {
+            $gallery->removeService($this);
+        }
+
+        return $this;
+    }
+}
